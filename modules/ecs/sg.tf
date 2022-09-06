aebb652bf506fe8e5865f5f891bc49bc94ce06f0
William Chrisp <william.chrisp@contino.io>
2022-09-06 18:01:29 +1000

    Add eggress rules to sg

--- modules/ecs/sg.tf
+++ modules/ecs/sg.tf
@@ -11,6 +11,14 @@ resource "aws_security_group" "alb_sg" {
     cidr_blocks      = ["0.0.0.0/0"]
   }
 
+  egress {
+    protocol         = "-1"
+    from_port        = 0
+    to_port          = 0
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
   tags = var.tags
 }
 
@@ -21,10 +29,18 @@ resource "aws_security_group" "ecs_sg" {
 
   ingress {
     description      = "Internet Web Traffic"
-    from_port        = 80
-    to_port          = 80
+    from_port        = 3000
+    to_port          = 3000
     protocol         = "tcp"
-    security_groups = [ aws_security_group.alb_sg.id ]
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  egress {
+    protocol         = "-1"
+    from_port        = 0
+    to_port          = 0
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = var.tags
